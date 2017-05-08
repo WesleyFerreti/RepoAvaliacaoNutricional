42b028b00fa87f921b2cdc32297c31436b771479
dennis <dennis@dennis-PC>
2017-05-08 17:38:08 -0300

    Escrevi Dennis

--- Telas/Login.php
+++ Telas/Login.php
@@ -1,5 +1,5 @@
 <?php
-echo "blabla";
+echo "Dennis";
 /* s
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
